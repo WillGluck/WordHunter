b82753cc2ff65fb3ce569f53d28c29dc85659f4d
Will Glück <bigwillgluck@gmail.com>
2014-11-13 20:09:06 -0200

    Tab da camera configurada, mais uns listeners setados.

--- WordHunter/src/br/furb/wordhunter/activities/HuntScreenActivity.java
+++ WordHunter/src/br/furb/wordhunter/activities/HuntScreenActivity.java
@@ -4,6 +4,10 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 import br.furb.wordhunter.fragments.ArchiveFragmentTab;
 import br.furb.wordhunter.fragments.LiveFragmentTab;
 import br.furb.wordhunter.listeners.WordHunterTabListener;
@@ -19,10 +23,15 @@ import com.example.wordhunter.R;
  */
 public class HuntScreenActivity extends FragmentActivity {
 	
+	//Tabs e fragments.
 	private ActionBar.Tab liveTab;
 	private ActionBar.Tab archiveTab;
 	private Fragment liveFragmentTab = new LiveFragmentTab();
 	private Fragment archiveFragmentTab = new ArchiveFragmentTab();
+	
+	//Componentes de tela que manteremos referência.
+	Button huntButton;
+	
 
 	@Override
 	protected void onCreate(Bundle arg0) {
@@ -30,6 +39,7 @@ public class HuntScreenActivity extends FragmentActivity {
 		setContentView(R.layout.activity_hunt_screen);
 		
 		this.initTabs();
+		this.initScreenComponents();
 				
 	}
 	
@@ -54,8 +64,32 @@ public class HuntScreenActivity extends FragmentActivity {
 		actionBar.addTab(this.liveTab);
 		actionBar.addTab(this.archiveTab);		
 	}
+		
+	/**
+	 * Inicializa os componentes de tela (carrega, seta listeners, e o que for necessário).
+	 */
+	private void initScreenComponents() {
+		this.huntButton = (Button) findViewById(R.id.hunt_screen_hunt_button);
+		this.huntButton.setEnabled(false);
+		this.huntButton.setOnClickListener(this.getOnHuntButtonClickListener());		
+	}	
 	
+	/**
+	 * @return OnClickListener do botão huntWord.
+	 */
+	private OnClickListener getOnHuntButtonClickListener() {
+		return new OnClickListener() {			
+			@Override
+			public void onClick(View arg0) {
+				HuntScreenActivity.this.huntWord();				
+			}
+		};
+	}
 	
-	
-	
+	/**
+	 * Ação de busca de palavra na imagem de input.
+	 */
+	private void huntWord() {
+		Toast.makeText(this, "Busca!", Toast.LENGTH_LONG).show();
+	}
 }
--- WordHunter/src/br/furb/wordhunter/fragments/LiveFragmentTab.java
+++ WordHunter/src/br/furb/wordhunter/fragments/LiveFragmentTab.java
@@ -1,8 +1,13 @@
 package br.furb.wordhunter.fragments;
 
+import java.io.IOException;
+
 import android.app.Fragment;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,12 +21,53 @@ import com.example.wordhunter.R;
  * @created 12/11/2014
  *
  */
-public class LiveFragmentTab extends Fragment {
+public class LiveFragmentTab extends Fragment implements TextureView.SurfaceTextureListener {
+		
+	private Camera camera;
+	private TextureView textureView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_live_tab, container, false);
+		View view = inflater.inflate(R.layout.fragment_live_tab, container, false);		
+		this.initCamera(view);		
 		return view;
+	}
+	
+	/**
+	 * Inicializa câmera e joga imagem no textureView.
+	 */
+	private void initCamera(View view) {
+		this.textureView = (TextureView) view.findViewById(R.id.live_tab_view);
+		this.textureView.setSurfaceTextureListener(this);
+		
+	}
+
+	@Override
+	public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {		
+		this.camera = Camera.open();
+		try {
+			this.camera.setPreviewTexture(surfaceTexture);
+			this.camera.startPreview();			
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+		this.camera.stopPreview();
+		this.camera.release();
+		return true;
+	}
+
+	@Override
+	public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+		//Nadica.
+	}
+
+	@Override
+	public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+		//Nadica.
 	}
 
 }
